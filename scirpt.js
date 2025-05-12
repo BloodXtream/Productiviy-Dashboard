853e56983bb1e28d237cd7a7fe7f0c2b88c0fd41
Pragyanam Tiwari <pragyanamtiwarii@gmail.com>
2025-05-12 22:51:33 +0530

    addTask completed

--- scirpt.js
+++ scirpt.js
@@ -15,4 +15,42 @@ const openFeatures = () => {
     })
 }
 
-openFeatures()
+// openFeatures()
+
+let form = document.querySelector('.addTask form')
+let taskInput = document.querySelector('.addTask form input')
+let taskDetailsInput = document.querySelector('.addTask form textarea')
+let taskCheckbox = document.querySelector('.addTask form #check')
+
+let currentTask = []
+
+const renderTask = () => {
+
+    let allTask = document.querySelector('.allTask')
+    let sum = ''
+
+
+    currentTask.forEach((elem) => {
+        sum += `<div class="task">
+                <h5>${elem.task}<span class=${elem.imp}>imp</span></h5>
+                <button>Done</button>
+            </div>`
+    })
+
+    allTask.innerHTML = sum
+}
+renderTask()
+
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault()
+    currentTask.push({
+        task: taskInput.value,
+        details: taskDetailsInput.value,
+        imp: taskCheckbox.checked
+    })
+    taskInput.value = ''
+    taskDetailsInput.value = ''
+    taskCheckbox.checked = ''
+    renderTask()
+})
